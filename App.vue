0c50114e97266d0863df809f636749e93374f7b6
we4reliving <muhammadiqbalkurniaadjie@gmail.com>
2025-05-29 20:53:53 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/orders">Pesanan Saya</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link> |
+      <router-link to="/admin">Admin</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
